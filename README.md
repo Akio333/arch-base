5ec72d9bc3dba72212fef54ee739aaf8f01458bc
Akio333 <smulye10@gmail.com>
2021-04-21 08:30:49 +0530

    cleaned install packages

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # arch-base
+
+If needed, load your keymap
+Refresh the servers with pacman -Syy
+Partition the disk
+Format the partitions
+Mount the partitions
+Install the base packages into /mnt (pacstrap /mnt base linux linux-firmware git vim intel-ucode (or amd-ucode))
+Generate the FSTAB file with genfstab -U /mnt >> /mnt/etc/fstab
+Chroot in with arch-chroot /mnt
+Download the git recpository with git clone https://github.com/Akio333/arch-install.git
+
+cd arch-install
+chmod +x base.sh
+run with ./base.sh
--- base.sh
+++ base.sh
@@ -22,7 +22,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 $hostvar.localdomain $hostvar" >> /etc/hosts
 
 # Base Install
-pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables iptables ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S --noconfirm grub efibootmgr dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb bash-completion openssh rsync reflector dnsmasq vde2 openbsd-netcat ebtables iptables ipset acpid os-prober ntfs-3g terminus-font
 
 # GPU Drivers installation
 # pacman -S --noconfirm xf86-video-amdgpu
@@ -33,16 +33,11 @@ grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Enable essantial services
-systemctl enable NetworkManager
-systemctl enable bluetooth
-systemctl enable cups
 systemctl enable sshd
 systemctl enable avahi-daemon
 systemctl enable tlp
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
-systemctl enable libvirtd
-systemctl enable firewalld
 systemctl enable acpid
 
 # Creating User
